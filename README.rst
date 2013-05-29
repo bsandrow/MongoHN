0e8e0e48a16e74c6d0e3eb993af72501d4695a89
Brandon Sandrowicz <brandon@sandrowicz.org>
2013-05-29 15:09:44 -0400

    Initial commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+MongoHN
+=======
+
+A HackerNews clone using Flask + MongoDB.
+
+Credits
+-------
+
+Copyright 2013 Brandon Sandrowicz <brandon@sandrowicz.org>
+
+License
+-------
+
+MIT License. See LICENSE.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+
+from glob import glob
+
+try:
+    from setuptools import setup
+except ImportError:
+    print("Falling back to distutils. Functionality may be limited.")
+    from distutils.core import setup
+
+requires = []
+long_description = open('README.rst').read() + "\n\n" + open("ChangeLog").read()
+
+config = {
+    'name'            : 'mongohn',
+    'description'     : 'A HackerNews-clone with a MongoDB backend.',
+    'long_description': long_description,
+    'author'          : 'Brandon Sandrowicz',
+    'author_email'    : 'brandon@sandrowicz.org',
+    'url'             : 'https://github.com/bsandrow/mongohn',
+    'version'         : '0.1',
+    'packages'        : ['mongohn'],
+    'package_data'    : { '': ['LICENSE'] },
+    'scripts'         : glob('bin/*'),
+    'install_requires': requires,
+    'license'         : open('LICENSE').read(),
+    'test_suite'      : 'mongohn.tests',
+}
+
+setup(**config)
